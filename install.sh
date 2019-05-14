0fa29d34c9569d86e2b368d8bba7f0a90e6767a4
Bill Abbott <babbott@perceval1.perceval.rutgers.edu>
2019-05-14 15:53:47 -0400

    script update

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ rm -f /etc/warewulf/vnfs/"$CHROOTDIR".conf
 wwmkchroot centos-7.5 $CHROOTHOME
 wwvnfs --chroot=$CHROOTHOME
 sed -i 's/^hybridpath.*/#\ &\nhybridize\ =\nhybrid\ =/' /etc/warewulf/vnfs/"$CHROOTDIR".conf
+echo add protected files
+rsync -av ../priv/ ./
+rsync -av ./ $CHROOTHOME/root/$WORKDIR/
 #rm -f $CHROOTHOME/etc/yum.repos.d/*
 #cp CentOS-Base.repo $CHROOTHOME/etc/yum.repos.d
 #mv $CHROOTHOME/etc/fstab $CHROOTHOME/etc/fstab-orig
@@ -21,7 +24,7 @@ sed -i 's/^hybridpath.*/#\ &\nhybridize\ =\nhybrid\ =/' /etc/warewulf/vnfs/"$CHR
 #rm -rf $CHROOTHOME/var/cache/yum
 #yum --installroot=$CHROOTHOME -y -q --releasever=7.5.1804 makecache
 #cp -r $STARTDIR $CHROOTHOME/root
-#chroot $CHROOTHOME /root/$WORKDIR/merge2.sh $WORKDIR
+#chroot $CHROOTHOME /root/$WORKDIR/prepimage.sh $WORKDIR
 #wwvnfs --chroot=$CHROOTHOME
 echo done on master
 
